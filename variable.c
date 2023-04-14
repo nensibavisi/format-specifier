eaa21a3db2fc271af549f089924f9dba1143cf1a
nensibavisi <130463985+nensibavisi@users.noreply.github.com>
2023-04-14 17:57:35 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+main()
+{
+	int a=5;
+	int b=7;
+	int c;
+	 c=a+b;
+	 printf("addition of %d&%d=%d\n",a,b,c);
+	 c=a-b;
+	 printf("substraction of %d&%d=%d\n",a,b,c);
+	 c=a*b;
+	 printf("multipication of %d&%d=%d\n",a,b,c);
+	 c=a/b;
+	 printf("division of %d&%d=%d\n",a,b,c);
+	 c=a%b;
+	 printf("modular of %d&%d=%d\n",a,b,c);
+	  
+	
+	
+	
+	
+	
+	
+	
+
+}
